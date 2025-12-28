0a6acc33b6aeacd3f00d22cb01b69ec68f8df06f
alonsorb777 <alonso.robredo@usach.cl>
2025-12-28 01:49:20 -0300

    Update documentacion.py

--- documentacion.py
+++ documentacion.py
@@ -1,9 +1,26 @@
 import math
 
+def limpiar_nan(datos): #para no tener problemas con los NaN creo esta funcion
+	"""
+devuelve una lista sin Nans
+	"""
+	datos_l = []
+
+	for i in datos:
+		if i ==i:
+			datos_l.append(i)
+
+	return datos_l
+
+
 def promedio(datos):
 	"""
 calcula el promedio de datos
 	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+	
     suma = 0
     for i in datos:
 		suma+=i
@@ -14,6 +31,10 @@ def mediana(datos):
 	"""
 calcula la mediana de los datos
 	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+	
     d_o=sorted(datos)
     n=len(d_o)
     medi= n//2 #la división tiene q ser un entero
@@ -41,6 +62,8 @@ def moda(datos):
 	"""
 calcula la moda (valor más repetido) de los datos
 	"""
+	datos = limpiar_nan(datos)
+	
     con =  {}
 
 	for i in con:
@@ -51,10 +74,15 @@ calcula la moda (valor más repetido) de los datos
 
 	return max(con, key=con.get) # esto permite no tener errores y contar correctamente lo que se busca para la moda
 
+
 def rango(datos):
 	"""
 calcula el rango de los datos
 	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+	
 	ma = max(datos)
 	mi = min(datos)
 
@@ -65,12 +93,58 @@ def varianza(datos):
 	"""
 calcula la varianza de los datos
 	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+		
 	suma = 0
 	mm = media(datos)
 
+	for i in datos:
+		suma+=(i-mm)**2
+
+	return suma/len(datos)
 
 
+def des_estandar(datos):
+	"""
+calcula La desviaciÓn estandar de los datos
+	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+		
+	return math.sqrt(varianza(datos))
 
+
+def precentil(datos,p):
+	"""
+calcula el precentil (p) ignora los NaN
+	"""
+	datos = limpiar_nan(datos)
+	if len(datos) ==0:
+		return float("nan")
+
+	d_o= sorted(datos)
+	n = len(d_o)
+
+	k = (n-1)*p/100
+	f =int(k)
+	g = f+1
+
+	if g >= n:
+		return d_0[f]
+
+	else:
+		return d_o[f] + (k-f)*(d_0[g]-d_o[f])
+
+
+def rang_intq(datos):
+	q1 = precentil(datos,25)
+	q3 = precentil(datos,75)
+
+	return q3-q1
+	
 
 
 
