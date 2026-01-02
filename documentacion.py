ef9915f00ca6d90be565e391df1498e2d5de525c
alonsorb777 <alonso.robredo@usach.cl>
2026-01-01 21:40:54 -0300

    documentacion codigo completo (limpiar_nan, promedio, mediana,desviacion_mediana_absoluta, moda, rango, varianza, des_estandar, precentil, rang_intq)

--- documentacion.py
+++ documentacion.py
@@ -1,53 +1,56 @@
 import math
 
 def limpiar_nan(datos): #para no tener problemas con los NaN creo esta funcion
-	"""
-devuelve una lista sin Nans
-	"""
-	datos_l = []
+    """
+    devuelve una lista sin Nans
+    """
+    datos_l = []
 
-	for i in datos:
-		if i ==i:
-			datos_l.append(i)
+    for i in datos:
+    	if i == i:
+    		datos_l.append(i)
 
-	return datos_l
+    return datos_l
 
 
 def promedio(datos):
-	"""
-calcula el promedio de datos
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
-	
-    suma = 0
+    """
+    calcula el promedio de datos
+    """
+    datos = limpiar_nan(datos)
+
+    if len(datos) ==0:
+    	return float("nan")
+
+
+    suma = 0 
     for i in datos:
-		suma+=i
+        suma+=i
     return suma/len(datos)
 
 
+
 def mediana(datos):
-	"""
-calcula la mediana de los datos
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
-	
+    """
+    calcula la mediana de los datos
+    """
+    datos = limpiar_nan(datos)
+    if len(datos) ==0:
+    	return float("nan")
+
     d_o=sorted(datos)
     n=len(d_o)
     medi= n//2 #la división tiene q ser un entero
 
     if n%2 == 0:
-		return(d_o[medi-1]+d_o[medi])/2
+    	return(d_o[medi-1]+d_o[medi])/2
     else:
-		return(d_o[medi])
+    	return(d_o[medi])
 
 
 def desviacion_mediana_absoluta(datos):
 	"""
-calcula la desviacion de la mediana absoluta
+    calcula la desviacion de la mediana absoluta
 	"""
 	med = mediana(datos)
 	desviacion = []
@@ -59,126 +62,95 @@ calcula la desviacion de la mediana absoluta
 
 
 def moda(datos):
-	"""
-calcula la moda (valor más repetido) de los datos
-	"""
-	datos = limpiar_nan(datos)
-	
-    con =  {}
+    """
+    calcula la moda (valor que mas se repite) en datos
+    """
+    datos = limpiar_nan(datos)
 
-	for i in con:
-		if i in con:
-			con[i] +=1
-		else:
-			con[i] = 1
+    if len(datos) == 0:
+        return float("nan")
 
-	return max(con, key=con.get) # esto permite no tener errores y contar correctamente lo que se busca para la moda
+    con = {} 
+
+    for i in datos:
+        if i in con:
+            con[i] += 1
+        else:
+            con[i] = 1
+
+    max_f = max(con.values())
+
+    if max_f == 1:
+        return float("nan")
+
+    return max(con, key=con.get)
 
 
 def rango(datos):
-	"""
-calcula el rango de los datos
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
-	
-	ma = max(datos)
-	mi = min(datos)
+    """
+    calcula el rango de datos (val_max-val_min) sin contar los nan
+    """
 
-	return ma-mi
+    datos = limpiar_nan(datos)
+    if len(datos) == 0:
+        return float("nan")
+
+    ma = max(datos)
+    mi = min(datos)
+
+    return ma - mi
 
 
 def varianza(datos):
-	"""
-calcula la varianza de los datos
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
-		
-	suma = 0
-	mm = media(datos)
+    """
+    calcula la varianza de los datos
+    """
+    datos = limpiar_nan(datos)
+    if len(datos) == 0:
+        return float("nan")
 
-	for i in datos:
-		suma+=(i-mm)**2
+    m = promedio(datos)
+    suma = 0
 
-	return suma/len(datos)
+    for i in datos:
+        suma+=(i-m)**2
+
+    return suma/len(datos)
 
 
 def des_estandar(datos):
-	"""
-calcula La desviaciÓn estandar de los datos
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
-		
-	return math.sqrt(varianza(datos))
+    """
+    calcula la desviacion estandar de datos
+    """
+    datos = limpiar_nan(datos)
+    if len(datos) == 0:
+        return float("nan")
+
+    return math.sqrt(varianza(datos)) #ya conocemos como calcular la varianza, no hay q hacerlo de nuevo
 
 
 def precentil(datos,p):
-	"""
-calcula el precentil (p) ignora los NaN
-	"""
-	datos = limpiar_nan(datos)
-	if len(datos) ==0:
-		return float("nan")
+    """
+    calcula el precentil (p) ignora los NaN de la forma "nearest-rank" que creo que es la mas conveniente
+    """
+    datos = limpiar_nan(datos)
+    if len(datos) == 0:
+        return float("nan")
 
-	d_o= sorted(datos)
-	n = len(d_o)
+    if p < 0 or p>100:
+        return float("nan")
 
-	k = (n-1)*p/100
-	f =int(k)
-	g = f+1
+    d_o = sorted(datos)
+    n = len(d_o)
 
-	if g >= n:
-		return d_0[f]
-
-	else:
-		return d_o[f] + (k-f)*(d_0[g]-d_o[f])
-
+    k = math.ceil(p*n/100)-1
+    return d_o[k]
 
 def rang_intq(datos):
-	"""
-Calcula el rango intercuartilico
-	"""
-	q1 = precentil(datos,25)
-	q3 = precentil(datos,75)
+    """
+    calcula el rango inter cuartil
+    """
+    q1 = precentil(datos,25)
+    q3 = precentil(datos,75)
 
-	return q3-q1
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return q3-q1
